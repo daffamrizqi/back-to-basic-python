781f74fe3034f7c8a549568122cfe91bac3d13d0
daffamrizqi <daffamrizqy@gmail.com>
2023-05-14 23:08:07 +0700

    wip linked list gpt

---
+++ ds-algo/linked-list/ll_gpt.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def is_empty(self):
+        return self.head is None
+    
+    def add_node(self, data):
+        new_node = Node(data)
+        if self.is_empty():
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def print_list(self):
+        current_node = self.head
+        while current_node is not None:
+            print(current_node.data, end=" -> ")
+            current_node = current_node.next
+        print("None")
+
+ll = LinkedList()
+for i in range(10):
+    ll.add_node(i)
+
+ll.print_list()
